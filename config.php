bd2464b27c2feee2fc1739b8d9a127fcbe85270d
Ani <ani-leo@outlook.com>
2018-01-03 01:49:20 +0000

    Config: Move to January 1st, 2018

--- config.php
+++ config.php
@@ -115,8 +115,9 @@ $a_histdates = array(
 	'2017_08' => array(array('y' => 2017, 'm' => 8,  'd' => 01),  array('y' => 2017, 'm' => 9,  'd' => 01)), 
 	'2017_09' => array(array('y' => 2017, 'm' => 9,  'd' => 02),  array('y' => 2017, 'm' => 9,  'd' => 30)),
 	'2017_10' => array(array('y' => 2017, 'm' => 10, 'd' => 01),  array('y' => 2017, 'm' => 11,  'd' => 02)),
-	'2017_11' => array(array('y' => 2017, 'm' => 11, 'd' => 03),  array('y' => 2017, 'm' => 11,  'd' => 30))
+	'2017_11' => array(array('y' => 2017, 'm' => 11, 'd' => 03),  array('y' => 2017, 'm' => 11,  'd' => 30)),
+	'2017_12' => array(array('y' => 2017, 'm' => 12, 'd' => 01),  array('y' => 2017, 'm' => 12,  'd' => 31))
 );
 
 // Current month
-$a_currenthist = array('2017_12', 'December 1st, 2017', '2017-12-01');
+$a_currenthist = array('2018_01', 'January 1st, 2018', '2018-01-01');
