6ea209a282eafa105988f4b0bfaa91467cf3131e
Ani <ani-leo@outlook.com>
2018-07-17 03:05:10 +0100

    Game: Tweaks
    - Allows for fetching games without commit and/or wiki information, for faster fetches on situations that don't require them;
    - Briefly documents existing functions;
    - Refactors Game Title / Date sorting, obliterates stupid code and reduces algorithm complexity.

--- objects/Game.php
+++ objects/Game.php
@@ -43,12 +43,12 @@ class Game {
     $this->status = $status;
     $this->date = $date;
 
-    if (!is_null($wiki)) {
+    if (!is_null($a_wiki) && !is_null($wiki)) {
       $this->wikiID = $wiki;
       $this->wikiTitle = $a_wiki[$wiki];
     }
 
-    if ($shortcommit == '0' || !array_key_exists(substr($shortcommit, 0, 7), $a_cache)) {
+    if (!is_null($a_cache) && ($shortcommit == '0' || !array_key_exists(substr($shortcommit, 0, 7), $a_cache))) {
       $this->commit = $shortcommit;
       $this->pr     = 0;
     } else {
@@ -76,31 +76,69 @@ class Game {
 
   }
 
+
+  /**
+    * rowToGame
+    * Obtains a Game from given MySQL Row.
+    *
+    * @param object  $row       The MySQL Row (returned by mysqli_fetch_object($query))
+    * @param object  $a_cache   Commit => PR cache
+    * @param object  $a_wiki    Wiki Page ID => Page Title cache
+    *
+    * @return object $game      Game fetched from given Row
+    */
   public static function rowToGame($row, &$a_cache, &$a_wiki) {
     return new Game($a_cache, $a_wiki, $row->game_title, $row->alternative_title, $row->status, $row->last_update, $row->wiki, $row->build_commit,
     $row->gid_EU, $row->tid_EU, $row->gid_US, $row->tid_US, $row->gid_JP, $row->tid_JP, $row->gid_AS, $row->tid_AS, $row->gid_KR, $row->tid_KR, $row->gid_HK, $row->tid_HK);
   }
 
-  public static function queryToGames($query) {
+  /**
+  	* queryToGames
+  	* Obtains array of Games from given MySQL Query.
+  	*
+  	* @param object  $query        The MySQL Query (returned by mysqli_query())
+  	* @param bool    $commitData   Whether to include commit related data
+  	* @param bool    $wikiData     Whether to include wiki related data
+  	*
+  	* @return object $array        Array of Games fetched from given Query
+  	*/
+  public static function queryToGames($query, $commitData = true, $wikiData = true) {
     $db = getDatabase();
-    $a_cache = file_exists(__DIR__.'/../cache/a_commits.json') ? json_decode(file_get_contents(__DIR__.'/../cache/a_commits.json'), true) : cacheCommitCache();
 
-    $a_wiki = array();
-    $q_wiki = mysqli_query($db, "SELECT `page_id`, `page_title` FROM `rpcs3_wiki`.`page` WHERE `page_namespace` = 0 ;");
-    while ($row = mysqli_fetch_object($q_wiki))
-      $a_wiki[$row->page_id] = $row->page_title;
+    if ($commitData) {
+      $a_cache = file_exists(__DIR__.'/../cache/a_commits.json') ? json_decode(file_get_contents(__DIR__.'/../cache/a_commits.json'), true) : cacheCommitCache();
+    } else {
+      $a_cache = NULL;
+    }
+
+    if ($wikiData) {
+      $a_wiki = array();
+      $q_wiki = mysqli_query($db, "SELECT `page_id`, `page_title` FROM `rpcs3_wiki`.`page` WHERE `page_namespace` = 0 ;");
+      while ($row = mysqli_fetch_object($q_wiki))
+        $a_wiki[$row->page_id] = $row->page_title;
+    } else {
+      $a_wiki = NULL;
+    }
+
+    mysqli_close($db);
 
     $a_games = array();
     while ($row = mysqli_fetch_object($query))
       $a_games[] = self::rowToGame($row, $a_cache, $a_wiki);
 
-    mysqli_close($db);
-
     return $a_games;
   }
 
-  // Types: 2 (Title), 3 (Status), 4 (Date)
-  // Orders: a (ASC), d (DESC)
+  /**
+    * sort
+    * Sorts an array of Games.
+    *
+    * @param object  $array   The Games array that needs sorting
+    * @param int     $type    Type of sorting (2:Title, 3:Status, 4:Date)
+    * @param string  $order   Order of sorting (a:ASC, d:DESC)
+    *
+    * @return object $array   Sorted array of Games
+    */
   public static function sort(&$array, $type, $order) {
     global $a_title;
 
@@ -110,42 +148,52 @@ class Game {
      * Game Title and Date
      */
     if ($type == 2 || $type == 4) {
-      // Temporary array to store game titles
-      $temp = array();
 
-      if ($type == 2)
-      foreach ($array as $game)
-        $temp[] = $game->title;
-      else foreach ($array as $game)
-        $temp[] = $game->date;
+      // Temporary arrays to store game titles and original keys respectively
+      $values = array();
+      $keys = array();
+
+      if ($type == 2) {
+        foreach ($array as $key => $game) {
+          $values[$key] = $game->title;
+          $keys[$key] = $game->title;
+        }
+      } else {
+        foreach ($array as $key => $game) {
+          $values[$key] = $game->date;
+          $keys[$key] = $game->date;
+        }
+      }
 
       // Alphabetical case-insensitive sort
-      natcasesort($temp);
+      natcasesort($values);
 
       // Reverse array if we want DESC order
       if ($order == 'd')
-        $temp = array_reverse($temp);
+        $values = array_reverse($values);
 
-      // Prevents an infinite loop hogging PHP incase something goes wrong
-      $exhaust = count($array)+1;
+      // Order all keys according to values' order (not really required)
+      $values = array_values($values);
 
-      while (!empty($temp) && 0 < $exhaust) {
-        $exhaust--;
-        $cur = array_shift($temp);
-        foreach ($array as $key => $game) {
-          if ($type == 2 ? $game->title == $cur : $game->date == $cur) {
-            $sorted[] = $game;
-            unset($array[$key]);
-            break;
-          }
-        }
+      // Original count of given array
+      $len = count($array);
+
+      // Move all entries from given array to a new sorted array in the correct order
+      for ($i = 0; $i < $len; $i++) {
+        $cur = array_shift($values);
+        $key = array_search($cur, $keys);
+        $sorted[$i] = $array[$key];
+        unset($array[$key]);
+        unset($keys[$key]);
       }
+
     }
 
     /*
      * Status
      */
     if ($type == 3) {
+
       if ($order == 'a') {
         $i = 1;
         $limit = count($a_title);
@@ -162,6 +210,7 @@ class Game {
           }
         }
       }
+
     }
 
     $array = $sorted;
