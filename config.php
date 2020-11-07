c7879d11670874afa1ae19064d507c71c78a6650
Ani <ani-leo@outlook.com>
2020-11-07 12:49:24 +0000

    Config: Move to November 1st, 2020

--- config.php
+++ config.php
@@ -200,8 +200,9 @@ $a_histdates = array(
 	'2020_06' => array(array('y' => 2020, 'm' => 6,  'd' => 01), array('y' => 2020, 'm' => 6,  'd' => 30)),
 	'2020_07' => array(array('y' => 2020, 'm' => 7,  'd' => 01), array('y' => 2020, 'm' => 7,  'd' => 31)),
 	'2020_08' => array(array('y' => 2020, 'm' => 8,  'd' => 01), array('y' => 2020, 'm' => 8,  'd' => 31)),
-	'2020_09' => array(array('y' => 2020, 'm' => 9,  'd' => 01), array('y' => 2020, 'm' => 9,  'd' => 30))
+	'2020_09' => array(array('y' => 2020, 'm' => 9,  'd' => 01), array('y' => 2020, 'm' => 9,  'd' => 30)),
+	'2020_10' => array(array('y' => 2020, 'm' => 10, 'd' => 01), array('y' => 2020, 'm' => 10, 'd' => 31))
 );
 
 // Current month
-$a_currenthist = array('2020_10', 'October 1st, 2020', '2020-10-01');
+$a_currenthist = array('2020_11', 'November 1st, 2020', '2020-11-01');
