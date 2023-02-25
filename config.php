cf8b4ed5bf9022caf4a8006110655aeffb2b619c
Ani <ani-leo@outlook.com>
2023-02-25 13:19:26 +0000

    Config: Move to February 1st, 2023

--- config.php
+++ config.php
@@ -237,8 +237,9 @@ $a_histdates = array(
 	'2022_09' => array(array('y' => 2022, 'm' => 9,  'd' => 01), array('y' => 2022, 'm' => 9,  'd' => 30)),
 	'2022_10' => array(array('y' => 2022, 'm' => 10, 'd' => 01), array('y' => 2022, 'm' => 10, 'd' => 31)),
 	'2022_11' => array(array('y' => 2022, 'm' => 11, 'd' => 01), array('y' => 2022, 'm' => 11, 'd' => 30)),
-	'2022_12' => array(array('y' => 2022, 'm' => 12, 'd' => 01), array('y' => 2022, 'm' => 12, 'd' => 31))
+	'2022_12' => array(array('y' => 2022, 'm' => 12, 'd' => 01), array('y' => 2022, 'm' => 12, 'd' => 31)),
+	'2023_01' => array(array('y' => 2023, 'm' => 1,  'd' => 01), array('y' => 2023, 'm' => 1,  'd' => 31))
 );
 
 // Current month
-$a_currenthist = array('2023_01', 'January 1st, 2023', '2023-01-01');
+$a_currenthist = array('2023_02', 'February 1st, 2023', '2023-02-01');
