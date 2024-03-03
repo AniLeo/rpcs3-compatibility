bb37649981f77618dc5fe23857e745716f51616c
Ani <ani-leo@outlook.com>
2024-03-03 12:39:08 +0000

    Config: Move to November 1st, 2023

--- config.php
+++ config.php
@@ -250,8 +250,9 @@ $a_histdates = array(
 	'2023_06' => array(array('y' => 2023, 'm' => 6,  'd' => 01), array('y' => 2023, 'm' => 6,  'd' => 30)),
 	'2023_07' => array(array('y' => 2023, 'm' => 7,  'd' => 01), array('y' => 2023, 'm' => 7,  'd' => 31)),
 	'2023_08' => array(array('y' => 2023, 'm' => 8,  'd' => 01), array('y' => 2023, 'm' => 8,  'd' => 31)),
-	'2023_09' => array(array('y' => 2023, 'm' => 9,  'd' => 01), array('y' => 2023, 'm' => 9,  'd' => 30))
+	'2023_09' => array(array('y' => 2023, 'm' => 9,  'd' => 01), array('y' => 2023, 'm' => 9,  'd' => 30)),
+	'2023_10' => array(array('y' => 2023, 'm' => 10, 'd' => 01), array('y' => 2023, 'm' => 10, 'd' => 31))
 );
 
 // Current month
-$a_currenthist = array('2023_10', 'October 1st, 2023', '2023-10-01');
+$a_currenthist = array('2023_11', 'November 1st, 2023', '2023-11-01');
