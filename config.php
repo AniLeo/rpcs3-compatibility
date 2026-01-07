d4c815eaca2101c561913d5700c14ed9de06e0fe
Ani <ani-leo@outlook.com>
2026-01-07 07:56:45 +0100

    Config: Move to December 1st, 2025

--- config.php
+++ config.php
@@ -324,8 +324,9 @@ $a_histdates = array(
     '2025_07' => array(array('y' => 2025, 'm' => 7,  'd' => 01), array('y' => 2025, 'm' => 7,  'd' => 31)),
     '2025_08' => array(array('y' => 2025, 'm' => 8,  'd' => 01), array('y' => 2025, 'm' => 8,  'd' => 31)),
     '2025_09' => array(array('y' => 2025, 'm' => 9,  'd' => 01), array('y' => 2025, 'm' => 9,  'd' => 30)),
-    '2025_10' => array(array('y' => 2025, 'm' => 10, 'd' => 01), array('y' => 2025, 'm' => 10, 'd' => 31))
+    '2025_10' => array(array('y' => 2025, 'm' => 10, 'd' => 01), array('y' => 2025, 'm' => 10, 'd' => 31)),
+    '2025_11' => array(array('y' => 2025, 'm' => 11, 'd' => 01), array('y' => 2025, 'm' => 11, 'd' => 30))
 );
 
 // Current month
-$a_currenthist = array('2025_11', 'November 1st, 2025', '2025-11-01');
+$a_currenthist = array('2025_12', 'December 1st, 2025', '2025-12-01');
